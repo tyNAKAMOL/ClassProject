da6947635139f596ac7c23801f833bb04b66c1ac
tyNAKAMOL <63010271@kmitl.ac.th>
2020-08-14 18:15:35 +0700

    commit2

---
+++ pattern/pattern/Sourcepp.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h> 
+
+int main() {
+    int r, k = 1;
+    scanf_s("%d", &r);
+    if (r > 0) {
+        for (int i = 0; i <= r; i++) {
+            for (int j = r - i; j >= 0; j--) {
+                printf(" ");
+            }
+            for (int j = 0; j < (2 * i) - j; j++, k++) {
+                printf("%d ", k % 10);
+            }printf("\n");
+        }
+        for (int i = r - 1; i >= 0; i--) {
+            for (int j = 0; j <= r - i; j++) {
+                printf(" ");
+            }
+            for (int j = 0; j < (2 * i) - j; j++, k++) {
+                printf("%d ", k % 10);
+            }printf("\n");
+        }
+    }
+    else printf("Error");
+    return 0;
+}
